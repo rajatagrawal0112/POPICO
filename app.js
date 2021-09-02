8a6a4c7016ae318839407ffd8ead0176a571e875
rajatagrawal0112 <rajatagrawal@questglt.org>
2021-09-02 15:23:16 +0530

    app.js

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ app.use('/', routes);
     res.setHeader('X-Foo', 'bar')
    })
 
-/*var options = {
+var options = {
 
 key: fs.readFileSync('/etc/letsencrypt/live/ebtico.com/privkey.pem', 'utf8'),
 
@@ -87,13 +87,13 @@ cert: fs.readFileSync('/etc/letsencrypt/live/ebtico.com/cert.pem', 'utf8'),
 
 ca: fs.readFileSync('/etc/letsencrypt/live/ebtico.com/chain.pem', 'utf8')
     
-};*/
+};
 
-// console.log('3009')
+console.log('3009')
 // Create an HTTP service.
-// http.createServer(app).listen(4000); 
+http.createServer(app).listen(4000); 
 // Create an HTTPS service identical to the HTTP service.
-// https.createServer(app).listen(443);
+https.createServer(app).listen(443);
 
 
  // Set 'views' directory for any views 
