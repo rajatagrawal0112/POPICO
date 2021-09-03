c73be933af85e4edb5eb464d12494d9c707ad4da
rajatagrawal0112 <rajatagrawal@questglt.org>
2021-09-03 17:24:40 +0530

    wallet created

--- controllers/blockchainController.js
+++ controllers/blockchainController.js
@@ -44,6 +44,7 @@ const verifyWallet = async (req, res) => {
     }
 }
 
+
 // const submitWallet = async (req, res) => {
 //     let user_id = req.session.re_us_id;
 //     let user_passphrase = req.body.passphrase.trim();
@@ -54,59 +55,39 @@ const verifyWallet = async (req, res) => {
 //         let address = await blockchainServices.checkWalletPrivate(user_passphrase);
 //         let UserwalletData = await blockchainServices.userWalletEntry(user_id, address, hash, created);
 //         if (UserwalletData) {
-//                 console.log("This is user wallet data",UserwalletData);
-//                 let walletData = blockchainServices.userWalletFindWallet(address);
-//                 let user = await userServices.checkUserId(user_id);
-//                 var sendReward = parseInt(signupReward);
-//                 if(user.ref_from){
-//                     console.log("This function is to check")
-//                     // let hashObject = await AdminCoinTransfer(address, referReward);
-//                     sendReward = sendReward + parseInt(referReward);
-//                     // let hash = hashObject.transactionHash;
-//                     // await blockchainServices.addTransaction(user_id, walletData._id, adminAddress, address, hash, referReward, 'ebt');
-//                     let userRefer = await userServices.checkUserReferCode(user.ref_from);
-//                     let subject = 'Referral bonus credited.'
-//                     let text = 'Hello '+ user.email + ',<br><br>\n\n' +
-//                      'Congratulations we have credited your $POP account by 5 $POP (worth US$5) as your friend signed up using your referral code!<br><br>\n\n' + 
-//                      'Earn more $POP by referring your friends and stand a chance to win exclusive $POP NFTs !!' + '<br><br>\n\n' + 'Regards,<br>\nTeam Abrand POP<br>\nhttps://pop.abrand.io/';
-//                     await mail(user.email, subject, text);
-//                     let userReferred = await userServices.checkUserWallet(userRefer._id);
-//                     let referAddress = userReferred.wallet_address;
-//                     let hashObject2 = await AdminCoinTransfer(referAddress, referReward);
-//                     let hash2 = hashObject2.transactionHash;
-//                     await blockchainServices.addTransaction(userRefer._id, userReferred._id, adminAddress, referAddress, hash2, referReward, '$POP');
-//                     if(hashObject2){
-//                         await userServices.refUpdate(user.ref_code, user.ref_from);
-//                     }
+//             let user = await userServices.checkUserId(user_id);
+
+//             if(user.ref_from){
+//                 await AdminCoinTransfer(address, referReward);
+//                 let userRefer = await userServices.checkUserReferCode(user.ref_from);
+//                 let subject = 'Referral bonus credited.'
+//                 let text = 'Hello '+ user.email + ',<br><br>\n\n' +
+//                 'Congratulations we have credited your $POP account by 10 $POP (worth US$10) as your friend signed up using your referral code!<br><br>\n\n' + 
+//                 'Earn more $POP by referring your friends and stand a chance to win exclusive ebt NFTs !!' + '<br><br>\n\n' + 'Regards,<br>\nTheebt Team<br>\nhttps://ebtico.com';
+//                 await mail(user.email, subject, text);
+//                 let userReferred = await userServices.checkUserWallet(userRefer._id);
+//                 let referAddress = userReferred.wallet_address;
+//                 let hashObject = await AdminCoinTransfer(referAddress, referReward);
+//                 if(hashObject){
+//                     await userServices.refUpdate(user.ref_code, user.ref_from);
 //                 }
-//                 var sendReward=parseInt(sendReward);
-//                 let finalSend = sendReward.toString();
-//                 console.log(finalSend);
-//                 console.log(address);
-//                 let hashObject3 = await AdminCoinTransfer(address, finalSend);
-//                 console.log(finalSend,'-------------------finalSend',typeof finalSend);
-//                 let hash3 = hashObject3.transactionHash;
-//                 await blockchainServices.addTransaction(user_id, walletData._id, adminAddress, address, hash3, finalSend, '$POP');
-//                 let userwallet = await blockchainServices.userWalletFindWallet(address);
-//                 await blockchainServices.importWalletEntry(user_id, userwallet._id, created);
-//                 res.redirect('/Create-wallet-success?wallet=' + Buffer.from(address).toString('base64'));
-            
 //             }
-//             else {
-//                 req.flash('err_msg', 'Something went wrong.');
-//                 res.redirect('/Create-wallet-dash');
-//             }
-//         // }
-//         // else {
-//         //     req.flash('err_msg', 'Something went wrong.');
-//         //     res.redirect('/Create-wallet-dash');
-//         // }
+//             console.log("reward",signupReward)
+//             await AdminCoinTransfer(address, signupReward);
+//             let userwallet = await blockchainServices.userWalletFindWallet(address);
+//             await blockchainServices.importWalletEntry(user_id, userwallet._id, created)
+//             res.redirect('/Create-wallet-success?wallet=' + Buffer.from(address).toString('base64'));
+//         }
+//         else {
+//             req.flash('err_msg', 'Something went wrong.');
+//             res.redirect('/Create-wallet-dash');
+//         }
+        
 //     }
 //     else {
 //         res.redirect('/verify-key');
 //     }
 // }
-
 
 const submitWallet = async (req, res) => {
     let user_id = req.session.re_us_id;
@@ -118,10 +99,12 @@ const submitWallet = async (req, res) => {
         let address = await blockchainServices.checkWalletPrivate(user_passphrase);
         let UserwalletData = await blockchainServices.userWalletEntry(user_id, address, hash, created);
         if (UserwalletData) {
+                console.log("This is user wallet data",UserwalletData);
                 let walletData = blockchainServices.userWalletFindWallet(address);
                 let user = await userServices.checkUserId(user_id);
-                let sendReward = parseInt(signupReward);
+                var sendReward = parseInt(signupReward);
                 if(user.ref_from){
+                    console.log("This function is to check")
                     // let hashObject = await AdminCoinTransfer(address, referReward);
                     sendReward = sendReward + parseInt(referReward);
                     // let hash = hashObject.transactionHash;
@@ -129,24 +112,27 @@ const submitWallet = async (req, res) => {
                     let userRefer = await userServices.checkUserReferCode(user.ref_from);
                     let subject = 'Referral bonus credited.'
                     let text = 'Hello '+ user.email + ',<br><br>\n\n' +
-                     'Congratulations we have credited your $EBT account by 5 $EBT (worth US$5) as your friend signed up using your referral code!<br><br>\n\n' + 
-                     'Earn more $EBT by referring your friends and stand a chance to win exclusive $EBT NFTs !!' + '<br><br>\n\n' + 'Regards,<br>\nTeam Abu Bakar<br>\nhttps://ebtico.com';
+                     'Congratulations we have credited your $POP account by 5 $POP (worth US$5) as your friend signed up using your referral code!<br><br>\n\n' + 
+                     'Earn more $POP by referring your friends and stand a chance to win exclusive $POP NFTs !!' + '<br><br>\n\n' + 'Regards,<br>\nTeam Abrand POP<br>\nhttps://pop.abrand.io/';
                     await mail(user.email, subject, text);
                     let userReferred = await userServices.checkUserWallet(userRefer._id);
                     let referAddress = userReferred.wallet_address;
                     let hashObject2 = await AdminCoinTransfer(referAddress, referReward);
                     let hash2 = hashObject2.transactionHash;
-                    await blockchainServices.addTransaction(userRefer._id, userReferred._id, adminAddress, referAddress, hash2, referReward, '$EBT');
+                    await blockchainServices.addTransaction(userRefer._id, userReferred._id, adminAddress, referAddress, hash2, referReward, '$POP');
                     if(hashObject2){
                         await userServices.refUpdate(user.ref_code, user.ref_from);
                     }
                 }
-                
+                var sendReward=parseInt(sendReward);
                 let finalSend = sendReward.toString();
+                console.log(finalSend);
+                console.log(address);
                 let hashObject3 = await AdminCoinTransfer(address, finalSend);
+
                 console.log(finalSend,'-------------------finalSend',typeof finalSend);
                 let hash3 = hashObject3.transactionHash;
-                await blockchainServices.addTransaction(user_id, walletData._id, adminAddress, address, hash3, finalSend, '$EBT');
+                await blockchainServices.addTransaction(user_id, walletData._id, adminAddress, address, hash3, finalSend, '$POP');
                 let userwallet = await blockchainServices.userWalletFindWallet(address);
                 await blockchainServices.importWalletEntry(user_id, userwallet._id, created);
                 res.redirect('/Create-wallet-success?wallet=' + Buffer.from(address).toString('base64'));
@@ -166,7 +152,6 @@ const submitWallet = async (req, res) => {
         res.redirect('/verify-key');
     }
 }
-
 
 
 // const sendCoin = async (req, res) => {
